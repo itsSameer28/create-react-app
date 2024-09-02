6914f2ecfafba8a62766056b6750147d940a9f10
Sameer <sharma100sameer>
2024-09-02 15:22:17 +0530

    hello from kmit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Abhi bhai !!!
+          Hello from Kmit
         </a>
       </header>
     </div>
